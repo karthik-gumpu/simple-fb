73342f0741a26e1c8095741c614a8ce4ff8cc7cb
Karthik Gumpu <karthik.gumpu@netenrich.com>
2018-12-04 22:42:11 +0530

    user profile page with posts added

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
-class UserProfile extends React.Component {
+import api from '../utils/api';
+import Loading from './Loading';
+import Posts from './Posts';
+
+const ProfileCard = React.memo(({ user }) => (
+    <div className="card text-center">
+        <h2> {user.name} </h2>
+        <h4> {user.username} </h4>
+        <i> {user.email} </i>
+        <a href={`${user.website}`}> {user.website}</a>
+        <p>
+            Company name: {user.company.name}
+        </p>
+    </div>
+));
+
+class UserProfile extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { user : null, showPosts: false }; 
+    }
+    componentDidMount() {
+        api({ url: `/users/${this.props.params.userId}`})
+        .then((user) => this.setState({ user }));
+    }
+    onHandleClick = () => {
+        this.setState((prevState) => ({ showPosts: !prevState.showPosts}));
+    }
     render() {
+        if(!this.state.user) {
+            return <Loading />
+        }
         return(
             <div>
-                this is user profile page
+                <ProfileCard user={this.state.user}/>
+                <button onClick={this.onHandleClick}>
+                    {this.props.showPosts ? 'Hide Posts' : 'Show Posts'}
+                </button>
+                <hr />
+                {
+                    this.state.showPosts &&
+                        <Posts {...this.props}/>
+                }
             </div>
         )
     }
