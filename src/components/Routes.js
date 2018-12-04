42b0e66c613d2a6428b05f7a15b7079760f38cd3
Karthik Gumpu <karthik.gumpu@netenrich.com>
2018-12-04 23:27:21 +0530

    routes updated

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory, IndexRedirect, useRouterHistory, hashHistory, Redirect } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect, Redirect } from 'react-router';
 
+import HomePage from './HomePage';
 import Posts from './Posts';
 import PostPage from './PostPage';
 import UserProfile from './UserProfile';
 import PageNotFound from './PageNotFound';
 
-const Routes =(
+const Routes = (
     <Router history={browserHistory}>
-        <IndexRedirect to="/posts/"/>
-        <Route
-            path="/posts/"
-            component={Posts}
-        />
-        <Route
-            path="/posts/:postId/"
-            component={PostPage}
-        />
-        <Route
-            path="/users/:userId/"
-            component={UserProfile}
-        />
-        <Route
-            path="/page-not-found/"
-            component={PageNotFound}
-        />
-        <Redirect from="*" to={"/page-not-found/"} />
+        <Route 
+            path="/"
+            component={HomePage}    
+        >
+            <IndexRedirect to="/posts/"/>
+            <Route
+                path="/posts/"
+                component={Posts}
+            />
+            <Route
+                path="/posts/:postId/"
+                component={PostPage}
+            />
+            <Route
+                path="/users/:userId/"
+                component={UserProfile}
+            />
+            <Route
+                path="/page-not-found/"
+                component={PageNotFound}
+            />
+        </Route>
+        <Redirect from="*" to="/page-not-found/" />
     </Router>
 )
 
